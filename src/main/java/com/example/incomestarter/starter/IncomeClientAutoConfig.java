9c974d5621d5d91b4e4622ab736760d3bdc3069f
ivan <i.gribanov@profinansy.net>
2025-08-31 23:08:45 +0300

    remove unused import

--- src/main/java/com/example/incomestarter/starter/IncomeClientAutoConfig.java
+++ src/main/java/com/example/incomestarter/starter/IncomeClientAutoConfig.java
@@ -3,7 +3,6 @@ package com.example.incomestarter.starter;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @AutoConfiguration
 public class IncomeClientAutoConfig {
